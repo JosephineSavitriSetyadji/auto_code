a44eb41c5fa638a935d267911c984c84b1eb635f
seveseria <emailkamu@example.com>
2025-11-25 09:59:53 +0700

    fix: update flask run config for railway

--- app.py
+++ app.py
@@ -176,4 +176,6 @@ def download(filename):
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    from os import environ
+    port = int(environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
